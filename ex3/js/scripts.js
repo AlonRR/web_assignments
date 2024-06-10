5a35b538866e74a78fa467757710234e06613371
Alon A. Rabinowitz <alonrab@outlook.com>
2024-06-10 15:33:41 +0300

    init commit of files of ex3

---
+++ ex3/js/scripts.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    initRectangles();
+    initSongs();
+    populateSongsInList();
+}
+
+function initRectangles() {
+}
+
+function initSongs() {
+}
+
+function chooseRectangleColor() {
+}
+
+function addRectangle() {
+}
+
+function subtractRectangle() {
+}
+
+function switchRectanglesSongs() {
+}
+
+function populateSongsInList() {
+}
